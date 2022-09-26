4b69d446c9057606ed16bb388995b712796f0aea
GlRose <garrett.rose03@gmail.com>
2022-09-25 20:49:52 -0400

    Get request to Edamam api food endpoint

--- server/server.js
+++ server/server.js
@@ -1,21 +1,46 @@
+const cors = require('cors')
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const axios = require('axios');
+
+
 require('dotenv').config()
 
-axios.get(`${process.env.BASE_URL}${process.env.API_KEY}`)
-    .then(response => {
-        console.log(response.data.foods[0].description)
+let PORT = process.env.PORT;
+app.use(cors())
+app.get("/", (req, res) => {
+    res.json({
+        name: "Garrett Rose",
+        age: 29
     })
-    .catch(error => {
-        console.log(error);
-    });
-app.listen(8080, () => {
-    console.log(`Server is running on port 8080.`);
+    console.log("connected")
 });
 
 
+app.get('/foods', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.edamam.com/api/food-database/v2/parser?',
 
+        params: {
+            app_id: process.env.API_ID,
+            app_key: process.env.API_KEY,
+            ingr: 'apple',
+        }
+    }
+    axios.request(options).then((response) => {
+        res.json(response.data)
+    }).catch((error) => {
+        console.error(error)
+    })
+})
+
+// app.post("/post", (req, res) => {
+//     console.log("Connected to Frontend");
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
