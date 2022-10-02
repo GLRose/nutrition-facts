bdfc04d4bf3be1190bea49f29295fbe617c107d9
GlRose <garrett.rose03@gmail.com>
2022-10-01 23:24:10 -0400

    App only renders componenets now

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,12 @@
-import axios from 'axios'
 import './App.css'
 import NutritionForm from './components/NutritionForm';
-import { useState, useEffect } from 'react';
+
 
 function App() {
-  const [food, setFood] = useState(null)
-
-
-
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      url: 'http://localhost:8080/foods',
-    }
-    axios.request(options).then((response) => {
-      console.log(response.data)
-      console.log(response.data.parsed.length)
-      if (response.data.parsed.length === 0) {
-        setFood(null);
-      } else {
-        setFood(response.data.text);
-      }
-    }).catch((error) => {
-      console.error(error)
-    })
-  }, [])
-
-  console.log(food);
-
   return (
 
     <div>
-      <NutritionForm food={food} />
-      {/* <p>{food}</p> */}
+      <NutritionForm />
     </div>
 
   )
